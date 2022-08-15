039263aeafb0c048ac3f646c38f9054b7688ed8a
asadalihaider <108663729+asadalihaider@users.noreply.github.com>
2022-08-15 13:55:54 +0500

    Added Router

--- public/index.js
+++ public/index.js
@@ -18,6 +18,10 @@ const app = express();
 
 app.use(express.json());
 
+const routes = require('../routes/routes');
+
+app.use('/api', routes)
+
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
